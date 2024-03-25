76aac2d00b99fe37db5d2c4f771886a627966b25
Edward Thomson <ethomson@edwardthomson.com>
2024-03-25 16:33:26 +0000

    Update from CI

--- index.js
+++ index.js
@@ -517,8 +517,8 @@ function parseJunitXml(xml) {
                     message = element.$.message;
                     details = element._;
                     let system_out;
-                    if (!details && (system_out = testcase["system-out"])) {
-                        details = system_out._;
+                    if (system_out = testcase["system-out"]) {
+                        details += "\n\nstdout:\n\n" + system_out._;
                     }
                     counts.failed++;
                 }
